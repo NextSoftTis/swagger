fcef83a009845c4d592e99e2c4fab0b348b1813b
payfit <nextsoft.tis@gmail.com>
2022-11-13 01:43:12 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,23 +1,13 @@
-var _rollbarConfig = {
-    accessToken: "{YOUR ROLLBAR ACCESS TOKEN}",
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    
-    payload: {
-      environment: "{YOUR ENVIRONMENT}",
-      client: {
-        javascript: {
-          source_map_enabled: true, // false by default
-          
-          // -- Add this into your configuration ---
-          code_version: "{CODE VERSION}",
-          // ---------------------------------------
-          
-          // Optionally have Rollbar guess which frames the error was 
-          // thrown from when the browser does not provide line 
-          // and column numbers.
-          guess_uncaught_frames: true
-        }
+const rollbarConfig = {
+  accessToken: YOUR_ACCESS_TOKEN,
+  captureUncaught: true,
+  payload: {
+    environment: process.env.REACT_APP_NODE_ENV,
+    client: {
+      javascript: {
+        source_map_enabled: true,
+        code_version: process.env.REACT_APP_GIT_SHA,
       }
     }
-  };
+  }
+}
